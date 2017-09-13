3fa728901cc09e41d0296a47366f265e1094003b
Yash Pungaliya <yashpungaliya@gmail.com>
2017-09-13 16:02:55 +0530

    Fix Parameter Input

--- SymbolTable.java
+++ SymbolTable.java
@@ -33,17 +33,16 @@ public class SymbolTable {
 		return newEntry;
 	}
 	public TableEntry lookup(String name,SymbolTable st) {
-		if(st==null)
-			return null;
-		for(TableEntry entry:st.entries) {
+			for(TableEntry entry:st.entries) {
 			if(entry.getId().equals(name)) {
 				System.out.println("\nEntry Found");
 				return entry;
 			}
 			System.out.println("\nNot found in current scope,searching in parent...");
-			return lookup(name,st.parent);
-				
+
 			}
+			if(st.parent!=null)
+				return lookup(name,st.parent);
 		return null;
 			
 	}
@@ -110,20 +109,22 @@ public class SymbolTable {
 								System.out.println("Enter parameter 1: \n\t<name>\n\t<type>");
 								String name2=s.next();
 								String type2=s.next();
-								st.entries.add(st.insert(name2, "par", type2));
+								String par="par";
+								st.entries.add(st.insert(name2, par, type2));
 								System.out.println("Parameter added");
 								}
 								else if(num_par==2) {
+									String par="par";
 									System.out.println("Enter parameter 1: \n\t<name>\n\t<type>");
 									String namepar1=s.next();
 									String typepar1=s.next();
-									st.entries.add(st.insert(namepar1, "par", typepar1));
+									st.entries.add(st.insert(namepar1, par, typepar1));
 									System.out.println("Parameter added");
 									
 									System.out.println("Enter parameter 2: \n\t<name>\n\t<type>");
 									String namepar2=s.next();
 									String typepar2=s.next();
-									st.entries.add(st.insert(namepar2, "par", typepar2));
+									st.entries.add(st.insert(namepar2, par, typepar2));
 									System.out.println("Parameter added");
 								}
 								else
